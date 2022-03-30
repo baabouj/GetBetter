0e2859be6d232a532a2934d3ef6279ad393dc6d5
Omar Baabouj <baabouj.dev@gmail.com>
2022-03-30 11:45:04 +0000

    :wave: Hello, World!

--- src/app.php
+++ src/app.php
@@ -2,6 +2,6 @@
 
 $app = \Pexess\Pexess::Application();
 
-$app->get("/",function (\Pexess\Http\Request $req,\Pexess\Http\Response $res){
+$app->get("/",function (\Pexess\Http\Request $req,\Pexess\Http\Response $res) {
     $res->status(200)->send("Hello, World!");
 });
