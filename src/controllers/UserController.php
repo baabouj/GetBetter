ccebd96bd5b6f13e927f60c802fc4547087e100f
Omar Baabouj <baabouj.dev@gmail.com>
2022-03-31 15:46:10 +0000

    :ok_hand: IMPROVE: Implement login and signup

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace app\controllers;
 
 use app\models\UserModel;
+use Pexess\Helpers\Hash;
+use Pexess\Helpers\StatusCodes;
 use Pexess\Http\Request;
 use Pexess\Http\Response;
 use Pexess\Models\Model;
@@ -18,12 +20,46 @@ class UserController
 
     public function signup(Request $req, Response $res)
     {
-        $res->send("Sign Up");
+        $body = $req->body();
+
+        $this->model->create([
+            "data" => [
+                "name" => $body["name"],
+                "email" => $body["email"],
+                "password" => Hash::hash($body["password"]),
+            ]
+        ]);
+
+        $res->status(StatusCodes::CREATED)->json(
+            [
+                "success" => true,
+                "message" => "User created successfully"
+            ]
+        );
     }
 
     public function login(Request $req, Response $res)
     {
-        $res->send("Log In");
+        $body = $req->body();
+        $user = $this->model->findUnique([
+            "where" => [
+                "email" => $body["email"]
+            ]
+        ]);
+
+        $is_correct_password = Hash::compare($body["password"], $user["password"]);
+
+        if ($is_correct_password) $res->status(StatusCodes::OK)->json([
+            "success" => true,
+            "message" => "User logged in"
+        ]);
+
+        $res->status(StatusCodes::BAD_REQUEST)->json([
+            "success" => false,
+            "errors" => [
+                "password" => "Invalid password"
+            ]
+        ]);
     }
 
 }
