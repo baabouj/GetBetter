baa2969bdd9912e5062a39a051ed921f05577cbc
Omar Baabouj <baabouj.dev@gmail.com>
2022-03-31 14:56:39 +0000

    :ok_hand: IMPROVE: Error handling: Handling 404 not found and 405 method not allowed http status codes errors

--- src/app.php
+++ src/app.php
@@ -1,5 +1,26 @@
 <?php
 
+use Pexess\Helpers\StatusCodes;
+use Pexess\Http\Request;
+use Pexess\Http\Response;
+
 $app = \Pexess\Pexess::Application();
 
 $app->group("/auth", require_once "src/routes/auth.php");
+
+
+// Error Handling
+
+$app->handle(StatusCodes::NOT_FOUND, function (Request $req, Response $res) {
+    $res->json([
+        "success" => false,
+        "message" => "Not Found"
+    ]);
+});
+
+$app->handle(StatusCodes::METHOD_NOT_ALLOWED, function (Request $req, Response $res) {
+    $res->json([
+        "success" => false,
+        "message" => "Method Not Allowed"
+    ]);
+});
