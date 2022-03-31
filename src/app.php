cccb2591610c476d0ae8cca1f23e2ea20cfdac3e
Omar Baabouj <baabouj.dev@gmail.com>
2022-03-31 14:54:32 +0000

    :ok_hand: IMPROVE: adding the auth group route

--- src/app.php
+++ src/app.php
@@ -2,6 +2,4 @@
 
 $app = \Pexess\Pexess::Application();
 
-$app->get("/",function (\Pexess\Http\Request $req,\Pexess\Http\Response $res) {
-    $res->status(200)->send("Hello, World!");
-});
+$app->group("/auth", require_once "src/routes/auth.php");
