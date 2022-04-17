bbe1864d2c99fe40630fbbaa2895f4e21b31d88c
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-17 13:36:28 +0000

    :lock: FIX: get token for the authorization header instead of the request body

--- backend/src/middlewares/AuthMiddleware.php
+++ backend/src/middlewares/AuthMiddleware.php
@@ -10,24 +10,26 @@ use Pexess\Validator\Validator;
 
 class AuthMiddleware implements \Pexess\Middlewares\Middleware
 {
-
     public function handler(Request $req, Response $res, $next)
     {
-        $token = JWT::verify($req->body()["token"] ?? "", $_ENV["JWT_SECRET_KEY"]);
-        if (!$token) {
+        $token = explode(" ", $req->headers()["authorization"])[1] ?? "";
+        $payload = JWT::verify($token, $_ENV["JWT_SECRET_KEY"]);
+
+        if (!$payload) {
             $res->status(StatusCodes::UNAUTHORIZED)->json([
                 "success" => false,
                 "message" => "Please provide a valid token"
             ]);
         }
-        $errors = Validator::validate(["id" => $token["userId"]], [
+
+        $errors = Validator::validate(["id" => $payload["userId"]], [
             "id" => "required|exist:users"
         ]);
         if ($errors) $res->status(StatusCodes::NOT_FOUND)->json([
             "success" => false,
             "message" => "No user found with that id"
         ]);
-        $req->user_id = $token["userId"];
+        $req->user_id = $payload["userId"];
         $next($req, $res);
     }
 }
