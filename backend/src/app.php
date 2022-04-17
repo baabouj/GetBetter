fd68d169c0774cf181fa24780460ebe88f56744b
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-17 13:38:59 +0000

    :ok_hand: FIX: use the get method to get user appointments instaed of post method

--- backend/src/app.php
+++ backend/src/app.php
@@ -7,9 +7,10 @@ use Pexess\Http\Response;
 $app = \Pexess\Pexess::Application();
 
 $app->group("/auth", require_once ROOT_DIR . "/src/routes/auth.php");
+
 $app->group("/appointments", require_once ROOT_DIR . "/src/routes/appointments.php");
 
-$app->route("/user/appointments")->post([\app\controllers\AppointmentsController::class, "getUserAppointments"])->apply(\app\middlewares\AuthMiddleware::class);
+$app->route("/user/appointments")->get([\app\controllers\AppointmentsController::class, "getUserAppointments"])->apply(\app\middlewares\AuthMiddleware::class);
 
 // Error Handling
 $app->handle(StatusCodes::NOT_FOUND, function (Request $req, Response $res) {
@@ -20,6 +21,8 @@ $app->handle(StatusCodes::NOT_FOUND, function (Request $req, Response $res) {
 });
 
 $app->handle(StatusCodes::METHOD_NOT_ALLOWED, function (Request $req, Response $res) {
+    // return 200 OK status if the method is options to allow browser to check CORS
+    if ($req->method() == "options") $res->status(200);
     $res->json([
         "success" => false,
         "message" => "Method Not Allowed"
